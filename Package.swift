ed8b7d71693d855bdc25ee9dbb8ebce0bb8c7603
dhruv <dhruv@tor.us>
2023-06-06 17:01:39 +0530

    ver bump fetch node details

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["SingleFactorAuth"])
     ],
     dependencies: [
-        .package(url: "https://github.com/torusresearch/fetch-node-details-swift.git", from: "4.0.0"),
+        .package(url: "https://github.com/torusresearch/fetch-node-details-swift.git", from: "4.0.1"),
         .package(url: "https://github.com/torusresearch/torus-utils-swift.git", from: "5.0.0"),
         .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0"),
         .package(name: "SessionManager", url: "https://github.com/Web3Auth/session-manager-swift.git", from: "2.0.0")

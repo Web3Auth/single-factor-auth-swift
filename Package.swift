8bc9c0187edf811ebff39bb90fc220779a70762d
shubham <16849485+metallicalfa2@users.noreply.github.com>
2023-06-22 17:05:56 +0800

    fix: package name

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .package(url: "https://github.com/torusresearch/fetch-node-details-swift.git", from: "4.0.1"),
         .package(url: "https://github.com/torusresearch/torus-utils-swift.git", from: "5.0.0"),
         .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0"),
-        .package(name: "SessionManager", url: "https://github.com/Web3Auth/session-manager-swift.git", from: "2.0.0")
+        .package(url: "https://github.com/Web3Auth/session-manager-swift.git", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,7 +28,7 @@ let package = Package(
             dependencies: [
                 .product(name: "FetchNodeDetails", package: "fetch-node-details-swift"),
                 .product(name: "TorusUtils", package: "torus-utils-swift"),
-                .product(name: "SessionManager", package: "SessionManager")
+                .product(name: "SessionManager", package: "session-manager-swift")
             ]),
         .testTarget(
             name: "SingleFactorAuthTests",

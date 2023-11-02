37ec91be1644ec26eebd94faca0d81f56e0e9408
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-11-02 07:34:29 +0200

    update dependency versions

--- Package.swift
+++ Package.swift
@@ -6,23 +6,21 @@ import PackageDescription
 let package = Package(
     name: "SingleFactorAuth",
     platforms: [
-        .iOS(.v14), .macOS(.v10_15)
+        .iOS(.v14), .macOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+
         .library(
             name: "SingleFactorAuth",
             targets: ["SingleFactorAuth"])
     ],
     dependencies: [
-        .package(url: "https://github.com/torusresearch/fetch-node-details-swift.git", from: "4.0.1"),
-        .package(url: "https://github.com/torusresearch/torus-utils-swift.git", from: "5.0.0"),
+        .package(url: "https://github.com/torusresearch/fetch-node-details-swift.git", from: "5.1.0"),
+        .package(url: "https://github.com/torusresearch/torus-utils-swift.git", from: "6.1.0"),
         .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0"),
-        .package(url: "https://github.com/Web3Auth/session-manager-swift.git", from: "2.0.0")
+        .package(url: "https://github.com/Web3Auth/session-manager-swift.git", from: "3.0.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SingleFactorAuth",
             dependencies: [

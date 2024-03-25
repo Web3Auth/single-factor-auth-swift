c23e7f14d94e7204882f376bac355df84abbade2
ieow <cwlee0@gmail.com>
2024-03-25 14:39:34 +0800

    fix: update torus-utils with latest tag 8.0.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/torusresearch/fetch-node-details-swift.git", from: "5.1.0"),
-        .package(url: "https://github.com/torusresearch/torus-utils-swift.git", from: "7.0.1"),
+        .package(url: "https://github.com/torusresearch/torus-utils-swift.git", from: "8.0.0"),
         .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0"),
         .package(url: "https://github.com/Web3Auth/session-manager-swift.git", from: "3.0.1")
     ],
